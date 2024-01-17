3fb097cec1303d7f7061a5a31716c7557c677f86
mkartikeyy <mkartikey666@gmail.com>
2024-01-17 10:40:40 +0530

    added files

---
+++ currency/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-white bg-green-400 rounded-md shadow-md w-full'>Kartikeymishra says hii</h1>
+    </>
+  )
+}
+
+export default App
---
+++ currency/src/hooks/value.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useCurrInfo(currency){
+    const [data, setData] = useState({})
+
+    useEffect(()=>{
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+    .then((res)=>{
+        return res.json()
+    })
+    .then(()=>{
+        return setData(res[currency])
+    })
+    },[currency])
+    return data
+}
+
+export default useCurrInfo;
